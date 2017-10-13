14cba82cc1fcefbbadb51c3b500991d61a7d87b8
Marius Steller Imingen <marius.steller@gmail.com>
2017-10-13 15:26:03 +0200

    NavigationDrawer complete as is. Navigation between history and main activity functional

--- app/src/main/java/com/example/imingen/workoutpal/HistoryActivity.java
+++ app/src/main/java/com/example/imingen/workoutpal/HistoryActivity.java
@@ -1,13 +1,34 @@
 package com.example.imingen.workoutpal;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class HistoryActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    NavigationDrawerFragment navigationDrawerFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        setUpDrawer();
+    }
+
+    private void setUpDrawer(){
+        navigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_drawer_fragment);
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationDrawerFragment.setUpDrawer(drawerLayout, toolbar, R.id.nav_history);
+    }
+
+    @Override
+    protected void onStart(){
+        navigationDrawerFragment.updateCheckedItem(R.id.nav_history);
+        super.onStart();
     }
 }
